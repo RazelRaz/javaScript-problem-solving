f4e0b002514b63f3b99933b8312b614638b5011f
RazelRaz <razel16dcm@gmail.com>
2023-06-20 20:28:00 +0600

    all question done

--- date-Object.js
+++ date-Object.js
@@ -1,3 +1,6 @@
+// 1.Date Object:
+//a. Create a function that takes a date in the format "YYYY-MM-DD" as a parameter and returns the day of the week for that date. The function should use the Date object.
+
 function getDayOfWeek(dateString) {
     const date = new Date(dateString);
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
